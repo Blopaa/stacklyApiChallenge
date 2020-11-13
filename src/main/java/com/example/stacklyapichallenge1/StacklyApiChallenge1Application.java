df25655bf04cff5a3b3d613548102e1c5422f7ee
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-14 00:12:46 +0100

    feat: init

---
+++ src/main/java/com/example/stacklyapichallenge1/StacklyApiChallenge1Application.java
@@ -0,0 +1,13 @@
+package com.example.stacklyapichallenge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StacklyApiChallenge1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StacklyApiChallenge1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stacklyapichallenge1/StacklyApiChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stacklyapichallenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StacklyApiChallenge1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
