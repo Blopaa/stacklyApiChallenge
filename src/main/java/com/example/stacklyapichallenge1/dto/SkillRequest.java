41baa43007a8c08503ec07bfd9e13fd985b44d2a
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-16 18:38:49 +0100

    feat: skillRequest

---
+++ src/main/java/com/example/stacklyapichallenge1/dto/SkillRequest.java
@@ -0,0 +1,13 @@
+package com.example.stacklyapichallenge1.dto;
+
+public class SkillRequest {
+    private String skillname;
+
+    public String getSkillname() {
+        return skillname;
+    }
+
+    public void setSkillname(String skillname) {
+        this.skillname = skillname;
+    }
+}
