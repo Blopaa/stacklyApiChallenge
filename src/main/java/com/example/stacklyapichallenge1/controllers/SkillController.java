8612078379f130cb143e1ed21171469fcab26c93
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-16 18:39:35 +0100

    feat: find skill by name

--- src/main/java/com/example/stacklyapichallenge1/controllers/SkillController.java
+++ src/main/java/com/example/stacklyapichallenge1/controllers/SkillController.java
@@ -1,14 +1,13 @@
 package com.example.stacklyapichallenge1.controllers;
 
+import com.example.stacklyapichallenge1.dto.SkillRequest;
 import com.example.stacklyapichallenge1.entities.Skill;
 import com.example.stacklyapichallenge1.services.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/skills")
@@ -17,9 +16,14 @@ public class SkillController {
     @Autowired
     private SkillService skillService;
 
-    @PostMapping("register")
+    @PostMapping("/register")
     public ResponseEntity<Skill> createSkill(@RequestBody Skill skill){
         return new ResponseEntity<>(skillService.createSkill(skill), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Skill> findById(@PathVariable String name){
+        return new ResponseEntity<>(skillService.findSkillByName(name), HttpStatus.FOUND);
     }
 
 }
