f88c3ec99149dcd42b6b4031cfd96840c7b5f8a4
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-16 18:43:30 +0100

    feat: AddSkillToUser

--- src/main/java/com/example/stacklyapichallenge1/repository/EmployeeRepository.java
+++ src/main/java/com/example/stacklyapichallenge1/repository/EmployeeRepository.java
@@ -1,8 +1,15 @@
 package com.example.stacklyapichallenge1.repository;
 
 import com.example.stacklyapichallenge1.entities.Employee;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
     Employee findByEmail(String email);
+    @Modifying
+    @Query(value = "insert into employee_skill (employee_id, skill_id) values (:employeeId, :skillId)", nativeQuery = true)
+    void AddSkillToUser(@Param(value = "employeeId") int employeeId , @Param(value = "skillId") int skillId);
+
 }
